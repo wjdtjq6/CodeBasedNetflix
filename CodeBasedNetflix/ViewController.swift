1f28a6c32730fee5e08b7d3668c8ab873635d382
jimmy <wjdtjq6@gmail.com>
2024-06-05 18:29:11 +0900

    Initial Commit

---
+++ CodeBasedNetflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeBasedNetflix
+//
+//  Created by t2023-m0032 on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
